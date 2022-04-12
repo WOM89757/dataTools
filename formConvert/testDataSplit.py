fa0b7f22d95443d7d808ebd65f827ae81d5f29c9
wangmao <wangcaowu@gmail.com>
2022-04-12 10:04:54 +0800

    feat(formconvert): auto get target name

--- formConvert/testDataSplit.py
+++ formConvert/testDataSplit.py
@@ -3,13 +3,14 @@ import argparse
 
 parser = argparse.ArgumentParser(description="eg. python testDataSplit.py --source inputtest.txt --target resulttest.txt")
 parser.add_argument("--source", type=str,  required=True,  help="source file eg. t.txt")
-parser.add_argument("--target",type=str,  required=True, help="target file eg. text.txt")
+# parser.add_argument("--target",type=str,  required=True, help="target file eg. text.txt")
 args = parser.parse_args()
 
 # print(args.source)
 
+target_name = args.source[:args.source.find('-') + 1] + 'result.txt'
 
-with open(args.target,"w") as f:
+with open(target_name, "w") as f:
     for line in open(args.source): 
         if line.find("####") != -1 :
             res = line.rsplit("/", 1)
